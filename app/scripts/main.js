66b501beab2197737f1cc17ba0240f99c58f5646
Your Name <you@example.com>
2016-01-29 19:37:58 +0200

    got the overlay working on phone, down to 360px

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,16 @@ $('.close-button').click(function () {
 $('#logo').click(function () {
     $('.overlay').addClass('overlay--cover');
 });
+
+$('.glyphicon-subtitles').click(function () {
+    $('.image__description').toggleClass('show');
+});
+
+$('.metadata-nav a').click(function () {
+    var tabPos = $(this).index();
+    console.log(tabPos);
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+
+    $('.metadata__tab--phone').removeClass('active').eq(tabPos).addClass('active');
+});
