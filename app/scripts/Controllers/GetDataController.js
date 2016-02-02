8e105ceafe0a02e6947c74979861778eb753ac5f
Hieu Nguyen <Blackevil245@gmail.com>
2016-02-02 23:05:58 +0200

    abc

--- app/scripts/Controllers/GetDataController.js
+++ app/scripts/Controllers/GetDataController.js
@@ -1,49 +1,52 @@
 angular.module('myApp', [])
-	.controller('GetDataController', function ($scope, $http, $sce) {
-		$scope.getImages = function () {
-			$http.get('http://util.mw.metropolia.fi/ImageRekt/api/v2/files/type/image').then($scope.imageSuccess, $scope.imageError);
-		};
-		$scope.imageSuccess = function (result) {
-			$scope.allImages = result.data;
-			console.log(result);
-		};
-		$scope.imageError = function (result) {
-			console.log(result);
-		};
-		$scope.getVideo = function () {
-			$http.get('http://util.mw.metropolia.fi/ImageRekt/api/v2/files/type/video').then($scope.videoSuccess, $scope.videoError);
-		};
-		$scope.videoSuccess = function (result) {
-			$scope.allVideo = result.data;
+    .controller('GetDataController', function($scope, $http, $sce) {
 
-			console.log(result);
-		};
-		$scope.videoError = function (result) {
-			console.log(result);
-		};
-		$scope.getAudio = function () {
-			$http.get('http://util.mw.metropolia.fi/ImageRekt/api/v2/files/type/audio').then($scope.audioSuccess, $scope.audioError);
-		};
-		$scope.audioSuccess = function (result) {
-			$scope.allAudio = result.data;
-			console.log(result);
-		};
-		$scope.audioError = function (result) {
-			console.log(result);
-		};
-	
-		//Load all content
-		$scope.runAll = function() {
-			$scope.getImages();
-			$scope.getVideo();
-			$scope.getAudio();
-			
-		};
+        var link = 'http://util.mw.metropolia.fi/uploads/'
 
-		//transform to trusted link for videos and audio
-		//Usa like this:
-		//<source ng-src="{{trustURL('http://util.mw.metropolia.fi/uploads/' + video.path)}}">
-		$scope.trustURL = function (videoUrl) {
-			return $sce.trustAsResourceUrl(videoUrl);
-		};
-	});
+        // Get Image
+        $scope.getImages = function() {
+            $http.get('http://util.mw.metropolia.fi/ImageRekt/api/v2/files/type/image')
+                .then(function(result) {
+                    $scope.allImages = result.data;
+                }, function(result) {
+                    console.log(result);
+                });
+        };
+
+        // Get Vid
+        $scope.getVideo = function() {
+            $http.get('http://util.mw.metropolia.fi/ImageRekt/api/v2/files/type/video')
+                .then(function(result) {
+                    $scope.allVideos = result.data;
+
+                    console.log(result);
+                }, function(result) {
+                    console.log(result);
+                });
+        };
+
+        //Get Audio
+        $scope.getAudio = function() {
+            $http.get('http://util.mw.metropolia.fi/ImageRekt/api/v2/files/type/audio')
+                .then(function(result) {
+                    $scope.allAudios = result.data;
+                    console.log(result);
+                }, function(result) {
+                    console.log(result);
+                });
+        };
+
+        //Load all content
+        $scope.runAll = function() {
+            $scope.getImages();
+            $scope.getVideo();
+            $scope.getAudio();
+        };
+
+        //transform to trusted link for videos and audio
+        //Usa like this:
+        //<source ng-src="{{trustURL('http://util.mw.metropolia.fi/uploads/' + video.path)}}">
+        $scope.trustURL = function(videoUrl) {
+            return $sce.trustAsResourceUrl(videoUrl);
+        };
+    });
