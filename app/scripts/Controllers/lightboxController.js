4af6d87589feb14c3a21e5b4013f2bca892facbd
shelune <em.hieunguyen@gmail.com>
2016-02-04 19:36:50 +0200

    working on single image linked to modal box

--- app/scripts/Controllers/lightboxController.js
+++ app/scripts/Controllers/lightboxController.js
@@ -3,16 +3,27 @@ angular.module('myApp')
         var lightbox = this;
 
         lightbox.metadataTab = 1;
+        lightbox.imgInfo();
 
         lightbox.selectTab = function (tab) {
             lightbox.metadataTab = tab;
         };
 
-        lightbox.tabSelected = function(checkTab) {
+        lightbox.tabSelected = function (checkTab) {
             return lightbox.metadataTab === checkTab;
         };
 
         $('.glyphicon-subtitles').click(function () {
             $('.image__description').toggleClass('show');
         });
+
+        var imgData = {};
+
+        lightbox.imgInfo = function () {
+            $('.image-content').attr('src', 'http://util.mw.metropolia.fi/upload' + imgData.path);
+            $('.image-info__name').text(imgData.title);
+            $('.image-info__upload-date').text(imgData.uploadTime);
+            $('.image-info__author').text(imgData.userId);
+            $('.image__description').find('p').text(imgData.description);
+        }
     });
