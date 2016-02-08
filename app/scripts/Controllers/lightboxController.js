7f5ebdfff004af01532d79252e7f52c12c03b263
Your Name <you@example.com>
2016-02-09 01:17:03 +0200

    fetching more data, working on file size

--- app/scripts/Controllers/lightboxController.js
+++ app/scripts/Controllers/lightboxController.js
@@ -15,12 +15,16 @@ angular.module('myApp')
         $('.glyphicon-subtitles').click(function () {
             $('.image__description').toggleClass('show');
         });
+
         $scope.$on('sendMedia', function () {
             var media = mediaFactory.mediaData;
             console.log(media);
             lightbox.lightboxOn = true;
             $('body').addClass('body--overlay');
             $('.overlay').addClass('overlay--cover');
+
+            var imgWidth, imgHeight, imgCamera, imgSize, imgBitDepth;
+
             $('.image__content').attr('src', 'http://util.mw.metropolia.fi/uploads/' + media.path);
             $('.image__description p').text(media.description);
             $('.image-info__name').text(media.title);
@@ -29,6 +33,24 @@ angular.module('myApp')
             $('.image-type').text(media.mimeType.substr(6).toUpperCase());
             $('.link.direct-link').val('http://util.mw.metropolia.fi/uploads/' + media.path);
             $('.link.html-link').val('<img src="http://util.mw.metropolia.fi/uploads/' + media.path + '">');
+
+            var image = new Image();
+            image.src = $('.image__content').attr('src');
+            EXIF.getData(image, function () {
+                    imgWidth = image.width || 'N/A',
+                    imgHeight = image.height || 'N/A',
+                    imgCamera = EXIF.getTag(image, "Model") || 'N/A';
+                console.log(imgWidth + ' . ' + imgHeight + ' . ' + imgCamera);
+                $('.resolution').text(imgWidth + ' x ' + imgHeight);
+                $('.camera').text(imgCamera);
+                // if (media.mimeType.substr(6).toUpperCase() === "JPEG") {
+                //     imgBitDepth = 24;
+                // } else if (media.mimeType.substr(6).toUpperCase() === "PNG") {
+                //     imgBitDepth = 32;
+                // }
+                // imgSize = imgBitDepth * imgWidth * imgHeight / 8 / 1024;
+                // $('.size').text(imgSize + ' KB');
+            });
         });
 
 
