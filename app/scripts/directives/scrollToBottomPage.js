a886bf10911de682941a11ace6b7f0fef0a227e7
Hieu Nguyen <Blackevil245@gmail.com>
2016-02-10 13:28:07 +0200

    fixed z-index

--- app/scripts/directives/scrollToBottomPage.js
+++ app/scripts/directives/scrollToBottomPage.js
@@ -1,16 +1,13 @@
 angular.module('myApp')
-	.directive('scrollToBottomPage', function($parse) {
-		return function($scope, element, attrs) {
-			var handler = $parse(attrs.scrollToBottomPage);
-			window.onscroll = function(evt) {
-				var scrollTop = element[0].scrollTop;
-				var scrollHeight = element[0].scrollHeight;
-				var offsetHeight = element[0].offsetHeight;
-				if (scrollTop === (scrollHeight - offsetHeight)) {
-					$scope.$apply(function() {
-						handler($scope);
-					});
-				}
-			};
-		};
-	});
+    .directive('scrollToBottomPage', function($parse) {
+        return function($scope, element, attrs) {
+            var handler = $parse(attrs.scrollToBottomPage);
+            window.onscroll = function(evt) {
+                if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                    $scope.$apply(function() {
+                        handler($scope);
+                    });
+                }
+            };
+        };
+    });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,5 @@
-
 (function() {
-    $('button[id^=signin-btn]').click(function () {
+    $('button[id^=signin-btn]').click(function() {
         $('body').addClass('body--overlay');
         $('.overlay').addClass('overlay--cover');
         $('.modal--login').addClass('modal-box--cover');
@@ -15,5 +14,11 @@
         $('body').addClass('body--overlay');
         $('.overlay').addClass('overlay--cover');
         $('.modal--upload').addClass('modal-box--cover');
+    });
+
+// Change Active state of nav
+    $('#nav-panel').children('a').click(function() {
+        $(this).children('i').toggleClass('active');
+        $(this).siblings('a').children('i').removeClass('active');
     });
 })();
