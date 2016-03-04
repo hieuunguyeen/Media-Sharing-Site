efcba006d976c475c002d6e13b79bb14fed3bda5
Togaa <you@example.com>
2016-03-04 14:36:27 +0200

    Audio player now auto hides and shows up instantly when starting a song. Added information to Profile page so it doesn't just show N/A

--- app/scripts/controllers/wavesurferController.js
+++ app/scripts/controllers/wavesurferController.js
@@ -7,41 +7,62 @@ angular.module('myApp')
         $scope.length = 0;
         $scope.currentTime = 0;
 
+        var updateTime = function () {
+            $scope.currentTime = new Date(1970, 0, 1).setSeconds(audioFactory.getCurrentTime());
+            $scope.$digest();
+        };
+
         $scope.$on('songReady', function () {
             $scope.length = new Date(1970, 0, 1).setSeconds(audioFactory.getDuration());
             $scope.playing = true;
             $scope.show = true;
             $('#waveform .loading-indication').css('opacity', 0);
             $('#waveform .loading-indication').css('width', 0 + '%');
-            $scope.update = setInterval(function() {
-                $scope.currentTime = new Date(1970, 0, 1).setSeconds(audioFactory.getCurrentTime());
-                $scope.$apply();
-            }, 1000);
-            $scope.$apply();
+            $scope.update = setInterval(updateTime, 1000);
+            $scope.startAutohide();
         });
 
         $scope.$on('songFinished', function () {
             $scope.playing = false;
-            $scope.$apply();
+            clearInterval($scope.update);
+            $scope.$digest();
         });
-        
+
         $scope.$on('buffering', function () {
+            $scope.show = true;
+            $scope.$digest();
             $('#waveform .loading-indication').css('opacity', 1);
             $('#waveform .loading-indication').css('width', audioFactory.percent + '%');
         });
-        /*$scope.$on('currentlyPlaying', function () {
-            $scope.currentTime = new Date(1970, 0, 1).setSeconds(audioFactory.getCurrentTime());
-            console.log($scope.currentTime);
-            $scope.$apply();
-        });*/
+
+        $scope.$on('seeking', function () {
+            updateTime();
+        });
+
+        $scope.startAutohide = function () {
+            $scope.autohide = setTimeout(function () { $scope.show = false; }, 3000);
+        };
+        $scope.cancelAutohide = function () {
+            clearTimeout($scope.autohide);
+        };
+
         $scope.showToggle = function () {
             $scope.show = !$scope.show;
+        };
+        $scope.loadSong = function (args) {
+            clearInterval($scope.update);
+            audioFactory.loadSong(args);
         };
         $scope.playPause = function () {
             audioFactory.playPause();
             $scope.playing = !$scope.playing;
+            if ($scope.playing == false) {
+                clearInterval($scope.update);
+            } else {
+                $scope.update = setInterval(updateTime, 500);
+            }
         };
-        
+
         $scope.muteToggle = function () {
             audioFactory.mute();
             $scope.mute = !$scope.mute;
