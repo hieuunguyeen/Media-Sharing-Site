5dc8de6c977a1e817668c3aa81533c7f3c20f126
Hieu Nguyen <Blackevil245@gmail.com>
2016-03-07 01:26:42 +0200

    remove infinite scroll, ready for presentation

--- app/scripts/directives/scrollToBottomPage.js
+++ app/scripts/directives/scrollToBottomPage.js
@@ -1,13 +1,13 @@
 angular.module('myApp')
     .directive('scrollToBottomPage', function($parse) {
-        return function($scope, element, attrs) {
-            var handler = $parse(attrs.scrollToBottomPage);
-            window.onscroll = function(evt) {
-                if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-                    $scope.$apply(function() {
-                        handler($scope);
-                    });
-                }
-            };
-        };
+        // return function($scope, element, attrs) {
+        //     var handler = $parse(attrs.scrollToBottomPage);
+        //     window.onscroll = function(evt) {
+        //         if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        //             $scope.$apply(function() {
+        //                 handler($scope);
+        //             });
+        //         }
+        //     };
+        // };
     });
